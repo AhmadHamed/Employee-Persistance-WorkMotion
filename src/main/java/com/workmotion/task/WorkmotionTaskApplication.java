389f79bddef5ec4c3c80b395f282467ab6d19c65
Ahmad Hamed <Ahmad_Hamed95@yahoo.com>
2021-12-21 00:17:24 +0200

    Init commit
    Signed-off-by: Ahmad Hamed <Ahmad_Hamed95@yahoo.com>

---
+++ src/main/java/com/workmotion/task/WorkmotionTaskApplication.java
@@ -0,0 +1,13 @@
+package com.workmotion.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkmotionTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkmotionTaskApplication.class, args);
+	}
+
+}
